ab8d1a1ecb28c208d2520609aefe0a125d856122
Willian Garcia <wgarcia1309@hotmail.com>
2017-03-13 18:44:10 -0500

    Zq

--- CodeTheory/src/codetheory/Controls/CodeTheory.java
+++ CodeTheory/src/codetheory/Controls/CodeTheory.java
@@ -1,10 +1,14 @@
 package codetheory.Controls;
 
 import codetheory.Models.Campo;
+import codetheory.Models.Conjunto;
 import java.util.Scanner;
 
 public class CodeTheory {
+
     public static void main(String[] args) {
+
+        /*
         int q,n,k,d;
         Scanner sc =new Scanner(System.in);
         System.out.println("Q");
@@ -14,8 +18,16 @@ public class CodeTheory {
         k=sc.nextInt();
         d=sc.nextInt();
         Campo c1 =new Campo(q,n);
-        System.out.println(c1.Vectores());
-     //zsubq es un campo? 
+        System.out.println(c1.Vectores());*/
+        Conjunto z = new Conjunto(3);
+        Conjunto a = new Conjunto(2);
+        Conjunto b = new Conjunto(1);
+        Conjunto c = new Conjunto(0);
+        Conjunto d = new Conjunto(500);
+        System.out.println(z.mostrar());
+        System.out.println(a.mostrar());
+        System.out.println(b.mostrar());
+        System.out.println(c.mostrar());
+        System.out.println(d.mostrar());
     }
-    
 }
---
+++ CodeTheory/src/codetheory/Models/Conjunto.java
@@ -0,0 +1,45 @@
+package codetheory.Models;
+import java.util.ArrayList;
+public class Conjunto {
+    /*
+    Conatructor del conjunto
+    Zq 
+    Sera por el momento un ArrayList,
+    con la posibilidad de cambiar a una lista
+    se pide solamente q.
+    */
+    private ArrayList puntos;
+    private int q;
+    
+    public Conjunto(int q) {
+        this.q = q;
+        puntos = new ArrayList();
+        if (q > 0) {
+            if (primo()) {
+                for (int i = 0; i < q - 1; i++) {
+                    puntos.add(i);
+                }
+            }
+        }
+    }
+    //verificacion si q es primo
+    private boolean primo() {
+        for (int i = 2; i < q - 1; i++) {
+            if (q % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //String  que muestra el conjunto de vectores, si q es 0 sera un conjunto vacio
+    public String mostrar() {
+        if (q != 0) {
+            String str = "{";
+            for (int i = 0; i <= q - 1; i++) {
+                str += i + ",";
+            }
+            return str.substring(0,str.length()-1 )+ "}";
+        }
+        return "{}";
+    }
+}

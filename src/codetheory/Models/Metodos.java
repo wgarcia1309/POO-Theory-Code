24e93504116ad3b94825138068684e383f8f417e
danielcatu <danielcatu@gmail.com>
2017-04-13 17:02:27 -0500

    Error Fatal

---
+++ src/codetheory/Models/Metodos.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codetheory.Models;
+
+/**
+ *
+ * @author hectordaniel
+ */
+public class Metodos {
+
+    /**
+     *
+     * @param k
+     * @param d se debe mandar d-1
+     */
+    public static void MatrixH(int k, int d, MatrixI M) {
+        Vector temp;
+        MatrixI aux;
+        int cont = 1, contd;
+        while (cont < k) {
+            contd = 0;
+            while (contd < d) {
+                M.getV()
+                contd++;
+            }
+        }
+    }
+
+    private static int suma(int cont, int k,MatrixI M) {
+        int x=0;
+        while (cont < k) {
+         x=(M.getV().getX()+x)%;
+         M=M.getLink();
+        }
+        return x;
+    }
+}
